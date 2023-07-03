e91b916270e4058dd288433d249282d8e689674a
hassanShakur <hass.shakurhassan@gmail.com>
2023-07-03 20:52:31 +0300

    Working popup functionality

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,28 @@ import * as view from './modules/view.js';
 import './style.css';
 import './modules/mobileMenuToggler.js';
 
+const mealsContainer = document.querySelector('main');
+const popupContainer = document.querySelector('.backdrop');
+
+let burgers;
+
 window.addEventListener('DOMContentLoaded', async () => {
-  const burgers = await api.getMeals();
+  burgers = await api.getMeals();
   view.renderMeals(burgers);
+});
+
+mealsContainer.addEventListener('click', (e) => {
+  const meal = e.target.closest('#meal');
+  if (!meal) return;
+
+  view.renderPopup(meal, burgers);
+});
+
+popupContainer.addEventListener('click', (e) => {
+  const isClosePopupIcon = e.target.closest('.back');
+  if (!isClosePopupIcon) return;
+
+  view.hidePopup();
 });
 
 //  (font-family: "Flame-Sans" style:normal weight:400 stretch:100 src index:0): status=2152398878 source: https://burgerking.ke/assets/fonts/FlameSans-Regular.woff2
--- client/src/modules/view.js
+++ client/src/modules/view.js
@@ -1,31 +1,92 @@
 import { getImage } from './api.js';
 
+import crossClosePopupImg from '../../images/close.png';
+import arrowClosePopup from '../../images/back.png';
+
 const mealsContainer = document.querySelector('main');
+const popupContainer = document.querySelector('.backdrop');
 
 export const renderCategory = (category) => {
-  const categoryMarkup = category.map(
-    (meal) => `
-        <div class="item">
+  const categoryMarkup = category
+    .map(
+      (meal, index) => `
+        <div class="item" id="meal" data-id="${index}" data-category="${
+  meal.category
+}">
             <img src="${getImage(meal.image)}" alt="${meal.name}">
             <p>${meal.name}</p>
         </div>
     `,
-  ).join('');
+    )
+    .join('');
 
   return categoryMarkup;
 };
 
 export const renderMeals = (meals) => {
-  const mealsMarkup = meals.map(
-    (category) => `
-        <section>
+  const mealsMarkup = meals
+    .map(
+      (category, index) => `
+        <section data-category="${index}">
             <h3>${category[0].category} menu</h3>
             <div class="content">
                 ${renderCategory(category)}
             </div>
         </section>
     `,
-  ).join('');
+    )
+    .join('');
 
   mealsContainer.innerHTML = mealsMarkup;
+};
+
+const generatePopupMarkup = (meal) => `
+  <div class="popup">
+    <div class="back">
+      <img src="${arrowClosePopup}" id="back-arrow" alt="Go back">
+      <img src="${crossClosePopupImg}" id="back-cross" alt="Go back">
+    </div>
+    <div class="popup-img">
+      <img src="${getImage(meal.image)}" alt="${meal.name}" />
+    </div>
+    <div class="meal">
+      <h3>${meal.name}</h3>
+      <div class="prices">
+        ${meal.prices
+    .map((price) => {
+      const mealPrices = Object.entries(price);
+      return `
+            <div class="price">
+              <span class="amount">${mealPrices[0][0]}</span>
+              <span class="value">Ksh. ${mealPrices[0][1]}</span>
+            </div>
+          `;
+    })
+    .join('')}
+      </div>
+
+      <div class="description">
+        <p>
+          ${meal.description}
+        </p>
+        <p class="combo">
+          ${meal.bundle}
+        </p>
+      </div>
+    </div>
+  </div>
+`;
+
+export const renderPopup = (meal, burgers) => {
+  const mealCategoryId = +meal.closest('section').dataset.category;
+  const mealId = +meal.dataset.id;
+
+  const mealData = burgers[mealCategoryId][mealId];
+  const popupMarkup = generatePopupMarkup(mealData);
+  popupContainer.classList.add('show-popup');
+  popupContainer.innerHTML = popupMarkup;
+};
+
+export const hidePopup = () => {
+  popupContainer.classList.remove('show-popup');
 };
